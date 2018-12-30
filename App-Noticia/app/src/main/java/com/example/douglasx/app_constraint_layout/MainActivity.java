1ce8150483f736176ef5b550ae73212df17a7a78
Douglas <douglasantana007@gmail.com>
2018-12-30 19:57:56 -0200

    [ADD] App Notícias
    Adicionado projeto de notícias a fins de entender como funciona o posicionamento de layout no desenvolvimento android.

---
+++ App-Noticia/app/src/main/java/com/example/douglasx/app_constraint_layout/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.douglasx.app_constraint_layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
